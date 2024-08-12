aed7900d7aa7e5507e0e66e5176ef25c74a6653e
anirudhan25 <anirudhanvijay@gmail.com>
2024-08-12 04:02:13 +0100

    Some API routes for stock items made
    - /from/supplier - gets all stock from a certain supllier
    
    - /item - returns an item by name
    
    - /like/item - returns a list of items with a similar name to the item in query (useful for a search bar)
    
    - /item/quantity - gets the quantity value of a specified item
    
    - /item/supplier - gets the supplier name of a specified item

--- app/backend/controllers/StockController.js
+++ app/backend/controllers/StockController.js
@@ -1,11 +1,10 @@
 const Stock = require('../models/StockModel');
 
-// get item 
-const getItem = async (req, res) => {
-    console.log('getting item...')  // for testing 
+
+const getStockBySupplier = async (req, res) => {
     try {
-        // querying the database for items with Supplier "Reynolds"
-        const items = await Stock.find({ Supplier: 'Reynolds' });
+        // querying the database for items with Supplier
+        const items = await Stock.find({ Supplier: req.params.supplier });
 
         // sending back the found items as a response
         res.json(items);
@@ -14,5 +13,45 @@ const getItem = async (req, res) => {
     }
 }
 
+const getStockByName = async (req, res) => {
+    try {
+        const items = await Stock.find({ Product: req.params.item });
+        res.json(items);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+const getSimilarStockByName = async (req, res) => {
+    try {
+        const items = await Stock.find({ Product: { $regex: req.params.item, $options: 'i' } });
+        res.json(items);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+const getQuantity = async (req, res) => {
+    try {
+        const items = await Stock.find({ Product: req.params.item });
+        quantities = items.map(item => item = item.Quantity);
+
+        res.json(quantities);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+const getSupplier = async (req, res) => {
+    try {
+        const items = await Stock.find({ Product: req.params.item });
+        suppliers = items.map(item => item = item.Supplier);
+
+        res.json(suppliers);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
 // export functions within an object
-module.exports = { getItem } 
+module.exports = { getStockBySupplier, getStockByName, getSimilarStockByName, getQuantity, getSupplier } 
--- app/backend/routes/stock.js
+++ app/backend/routes/stock.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // controller functions
-const {getItem} = require('../controllers/StockController');
+const { getStockBySupplier, getStockByName, getSimilarStockByName, getQuantity, getSupplier } = require('../controllers/StockController');
 
 // define routes
-router.get('/get', getItem)
+router.get('/from/:supplier', getStockBySupplier);
+router.get('/:item', getStockByName);
+router.get('/like/:item', getSimilarStockByName);
+router.get('/:item/quantity', getQuantity);
+router.get('/:item/supplier', getSupplier);
 
 module.exports = router;
