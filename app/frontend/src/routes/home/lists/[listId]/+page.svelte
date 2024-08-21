f871590b44b37537b1db178cc9422439ab4f02af
anirudhan25 <anirudhanvijay@gmail.com>
2024-08-21 10:33:05 +0530

    stock data now works in table view, working on the navigation bar / filters / search functions etc.
    need to do after:
    - home page
    - main database view
    - fixing lists table in database
    - adding custom lists
    - saving custom lists
    - exporting lists as excel sheets / text list

--- app/frontend/src/routes/home/lists/[listId]/+page.svelte
+++ app/frontend/src/routes/home/lists/[listId]/+page.svelte
@@ -9,6 +9,13 @@
   let products = [];
   let fadeIn = false;
   let selectedItems = []; // using an array for selected items
+  const supplierColors = {
+    'Bidfood': '#eb3617',
+    'Reynolds': '#FF5733',
+    'Birtwistles': '#4e0b58',
+    'DeliceDeFrance': '#3357FF',
+    'DestinyFoods': '#3299c5'
+  };
 
   const getFromSupplier = async () => {
     try {
@@ -35,6 +42,10 @@
     }
   };
 
+  const getBadgeColor = (supplier) => {
+    return supplierColors[supplier];
+  };
+
   onMount(() => {
     setTimeout(() => {
       fadeIn = true;
@@ -49,7 +60,7 @@
     position: absolute;
     height: 100vh;
     width: 100vw;
-    background-color: #033839;
+    background-color: #032939;
     z-index: -3;
   }
 
@@ -79,7 +90,7 @@
     left: 0;
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.55);
+    background: rgba(0, 0, 0, 0.67);
     z-index: -1; 
   }
 
@@ -89,8 +100,8 @@
     color: white;
     opacity: 0;
     transition: opacity 1s ease-in-out;
-    overflow-y: auto;
-    max-height: 85vh;
+    overflow-y: auto; /* enable vertical scrolling */
+    max-height: 85vh; /* max height to control the scrollable area */
   }
 
   .fade-in {
@@ -98,21 +109,30 @@
   }
 
   .table-container {
-    position: relative;
     overflow-y: auto;
-    max-height: 80vh;
+    max-height: 80vh; /* max height of the table container */
+    margin-top: 3vh;
   }
 
-  /* Gradient overlay to suggest scrolling */
-  .scroll-indicator::after {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    left: 0;
+  table {
+    border-collapse: separate;
+    border-spacing: 3px; /* adds space around the outside of cells */
     width: 100%;
-    height: 30px;
-    background: linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.8));
-    pointer-events: none;
+  }
+
+  th, td {
+    padding: 15px; /* increased space inside each cell */
+    border: 1px solid rgb(73, 109, 90);
+    background-color: #26342c;
+    color: #d1dfda;
+    text-align: center;
+    vertical-align: middle;
+    border-radius: 2px; /* rounded corners for cells */
+  }
+
+  th {
+    background-color: #2f4036;
+    font-size: 1.2em
   }
 
   .selected {
@@ -123,18 +143,18 @@
     cursor: pointer;
     width: 40px;
     height: 40px;
-    border: 1px solid #ccc;
+    border: 3px solid #d1dfda;
     border-radius: 50%;
-    background-color: #fff;
+    background-color: #ffffff;
     display: flex;
     align-items: center;
     justify-content: center;
-    transition: background-color 0.3s;
+    transition: background-color 0.15s;
     font-size: 1.2em;
   }
 
   .toggle-button.selected {
-    background-color: #000000;
+    background-color: #23ab37;
     color: #fff;
   }
 
@@ -143,7 +163,7 @@
     color: #15432a;
     border-radius: 10px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    transition: transform 0.25s ease, background-color 0.25s ease;
+    transition: transform 0.1s ease, background-color 0.1s ease;
     font-weight: 525;
   }
 
@@ -160,17 +180,41 @@
   .card dd {
     margin-top: 0.5em;
   }
+
+  .badge {
+    margin: 0;
+    padding: 7px 13px;
+    border-radius: 0.5vw;
+    font-size: 2.2vh;
+    font-family: Arial;
+    letter-spacing: 0.5px;
+    color: #d1dfda;
+    display: inline-block;
+  }
+
+  .no-spacing td {
+    margin: 0;
+    padding: 0;
+  }
+
+  .spacing-x {
+    margin-left: 0;
+    margin-right: 0;
+  }
 </style>
+
 
 
 <div class="background {fadeIn ? 'fade-in' : ''}">
   <div class="background-wrapper {fadeIn ? 'fade-in' : ''}">
     <div class="background-overlay"></div>
-    <div class="content {fadeIn ? 'fade-in' : ''} ml-[2vw]">
-      <h1 class="font-serif text-[6vh] mt-[4vh]">{listId}</h1>
+    <div class="content {fadeIn ? 'fade-in' : ''} ml-[3vw] mr-[3vw] pl-[5vw] pr-[5vw]">
+
+      <h1 class="font-serif text-[6vh] mt-[4vh] backdrop-blur-lg bg-[#477159] bg-opacity-30 h-[7vh]">{listId}</h1>
+
       {#if products.length > 0}
-        <div class="table-container scroll-indicator">
-          <table class="table-compact w-full mt-[5vh]">
+        <div class="table-container">
+          <table class="table-compact w-full mt-[1vh] no-spacing">
             <thead>
               <tr>
                 <th>Select</th>
@@ -181,16 +225,17 @@
             </thead>
             <tbody>
               {#each products as product}
-                <tr>
-                  <td>
+                <tr class="spacing-x">
+                  <td class="flex justify-center">
                     <button 
                       class={`toggle-button ${selectedItems.includes(product.Product) ? 'selected' : ''}`} 
                       on:click={() => toggleSelection(product)}>
                     </button>
                   </td>
-                  <td>{product.Product}</td>
-                  <td>{product.Quantity}</td>
-                  <td>{product.Supplier}</td>
+                  <td class="font-semibold bg-[#26342c] rounded-lg pl-[2vw] border-[#000000] border-md">{product.Product}</td>
+                  <td class="bg-[#26342c] rounded-lg border-[#000000] border-md">{product.Quantity}</td>
+                  <td class="bg-[#26342c] rounded-lg border-[#000000] border-md"><span class="badge"
+                    style="background-color: {getBadgeColor(product.Supplier)}">{product.Supplier}</span></td>
                 </tr>
               {/each}
             </tbody>
